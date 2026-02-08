8b7f4591de91b841fae43de63218d026b61cc759
victor zhang <victorwzhang1@gmail.com>
2026-02-07 21:26:01 -0800

    Update page.py

--- lstore/page.py
+++ lstore/page.py
@@ -7,29 +7,22 @@ class Page:
     CAPACITY = PAGE_SIZE // INT_SIZE  # 一页
     __slots__ = ("num_records", "data")
     def __init__(self):
-        #已写入记录数
         self.num_records = 0
-        #存整数，bytearray加struct太垃圾了
         self.data = []
+
     def has_capacity(self) -> bool:
         return self.num_records < self.CAPACITY
+
     def write(self, value: int) -> int:
-        """
-        追加一个64bit int, 返回该值页内offset
-        """
-        if self.num_records >= self.CAPACITY:
-            raise OverflowError("Page is full")
-        # 给None兼容
+        ...
+    
+    def read(self, offset: int) -> int:
+        ...
+
+    def overwrite(self, offset: int, value: int) -> None:
+        #（二次修改）覆盖写，update base 的 indirection/scheme
+        if offset < 0 or offset >= self.num_records:
+            raise IndexError("Offset out of range in Page")
         if value is None:
             value = 0
-        offset = self.num_records
-        self.data.append(int(value))
-        self.num_records += 1
-        return offset
-    def read(self, offset: int) -> int:
-        """
-        读取
-        """
-        if offset < 0 or offset >= self.num_records:#边界
-            raise IndexError("Offset out of range in Page")
-        return self.data[offset]
+        self.data[offset] = int(value)
