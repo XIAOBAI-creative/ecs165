b461c658e805aa099b47ab72652435d783d3f45c
victor zhang <victorwzhang1@gmail.com>
2026-02-10 21:01:45 -0800

    Update db.py

--- lstore/db.py
+++ lstore/db.py
@@ -14,9 +14,9 @@ class Database():
 
     """
     # Creates a new table
-    :param name: string         #Table name
-    :param num_columns: int     #Number of Columns: all columns are integer
-    :param key: int             #Index of table key in columns
+    :param name: string #Table name
+    :param num_columns: int #Number of Columns: all columns are integer
+    :param key: int #Index of table key in columns
     """
     def create_table(self, name, num_columns, key_index):
         table = Table(name, num_columns, key_index)
